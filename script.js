27e61b1a7a1fe337ad91972066ca7191ad2672e9
HeberSXT <heber.cardoso@somaasset.com.br>
2024-06-21 15:58:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarLinks = document.querySelectorAll('#navbar a');
+
+    navbarLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = event.target.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetSection.offsetTop - 60,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
 
